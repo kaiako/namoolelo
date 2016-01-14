d3c55ec9b0f766ed5f9b85ea0c585b40065f8b12
jordan <jordan@ehawaii.gov>
2016-01-14 10:23:40 -1000

    moved locationDao imple to dao package

---
+++ src/main/java/com/namoolelo/dao/LocationDaoImpl.java
@@ -0,0 +1,9 @@
+package com.namoolelo.dao;
+
+import com.namoolelo.domain.locations.Location;
+
+public class LocationDaoImpl extends GenericDaoImpl<Location, Long> implements LocationDao {
+	
+
+
+}
